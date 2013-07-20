a8112924e7eed445a375b8d9f41ef9d54f090132
Rohan Singh <rohan-singh@hotmail.com>
2013-07-20 10:15:23 -0400

    Create project files

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var window = new RenderWindow(new VideoMode(1280, 720), "AdventureGame", Styles.Close);
+            window.SetFramerateLimit(60);
+
+            window.Closed += (sender, eventArgs) => window.Close();
+
+            while (window.IsOpen())
+            {
+                window.DispatchEvents();
+
+                window.Clear(Color.Black);
+
+                window.Display();
+            }
+        }
+    }
+}
